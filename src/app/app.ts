6f0abcc826136c003a9cae157711e6deb9b1f9d6
GustavoDiego <121072900+GustavoDiego@users.noreply.github.com>
2026-05-12 16:23:05 -0300

    chore: disable sections and style updates; prepare GitHub Pages deploy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
-import { IntroductionComponent } from './components/introduction/introduction.component';
 import { AboutComponent } from './components/about/about.component';
-import { ApproachComponent } from './components/approach/approach.component';
-import { ForWhomComponent } from './components/for-whom/for-whom.component';
 import { ReflectionsComponent } from './components/reflections/reflections.component';
-import { PernambucoComponent } from './components/pernambuco/pernambuco.component';
 import { CredentialsComponent } from './components/credentials/credentials.component';
 import { CtaComponent } from './components/cta/cta.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -17,12 +13,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     HeaderComponent,
     HeroComponent,
-    IntroductionComponent,
     AboutComponent,
-    ApproachComponent,
-    ForWhomComponent,
     ReflectionsComponent,
-    PernambucoComponent,
     CredentialsComponent,
     CtaComponent,
     FooterComponent
