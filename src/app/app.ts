f40b71c6d06b316a3f23081644388e68bb2641c6
GustavoDiego <121072900+GustavoDiego@users.noreply.github.com>
2026-07-17 14:40:28 -0300

    fix: update whatsapp link text for clarity and consistency across components

--- src/app/app.ts
+++ src/app/app.ts
@@ -22,5 +22,5 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class App {
   protected readonly title = 'Dayse Silva';
-  whatsappLink = 'https://wa.me/5581996656998?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20uma%20consulta.';
+  whatsappLink = 'https://wa.me/5581996656998?text=Ol%C3%A1!%20Gostaria%20de%20entender%20como%20funcionam%20as%20sess%C3%B5es%20de%20psican%C3%A1lise%20com%20voc%C3%AA';
 }
--- src/app/components/cta/cta.component.ts
+++ src/app/components/cta/cta.component.ts
@@ -9,5 +9,5 @@ import { FadeInDirective } from '../../directives/fade-in.directive';
   styleUrl: './cta.component.scss'
 })
 export class CtaComponent {
-  whatsappLink = 'https://wa.me/5581996656998?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20uma%20consulta.';
+  whatsappLink = 'https://wa.me/5581996656998?text=Ol%C3%A1!%20Gostaria%20de%20entender%20como%20funcionam%20as%20sess%C3%B5es%20de%20psican%C3%A1lise%20com%20voc%C3%AA';
 }
